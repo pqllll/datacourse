cae689bbdffd4d42c31764763a34b67707daca94
zhn <zhn@great.mail>
2023-09-21 21:30:48 +0800

    v1.0

---
+++ zuoye/src/main/java/com/controller/helloController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class helloController {
+    @RequestMapping
+    public String hello(){
+
+        return "hello world!";
+    }
+}
---
+++ zuoye/src/main/java/com/zuoyeApp.java
@@ -0,0 +1,20 @@
+package com;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan("com.mapper")
+@ServletComponentScan
+@EnableTransactionManagement
+public class zuoyeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(zuoyeApp.class,args);
+        log.info("项目启动");
+    }
+}
